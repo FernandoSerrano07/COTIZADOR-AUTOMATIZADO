2d810d9737cd7bcf5889fbe9eed0d4748f06ab92
FernandoSerrano07 <134645378+FernandoSerrano07@users.noreply.github.com>
2026-09-09 17:54:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def verify_password(plain_password, hashed_password):
 
 @app.get("/", response_class=HTMLResponse)
 def read_index(request: Request):
-    return templates.TemplateResponse(request, "index.html", {"request": request})
+    return templates.TemplateResponse(request, "dashboard.html", {"request": request})
 
 
 @app.get("/login", response_class=HTMLResponse)
